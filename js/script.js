b1414792aaebc7c622fb7b6035584d78aceb0cf4
driyant <driyant.mail@gmail.com>
2021-04-16 07:08:28 +0700

    Inital coomit, modify js retrive data using async

--- js/script.js
+++ js/script.js
@@ -2,3 +2,12 @@ const button = document.querySelector("button");
 const authorSpan = document.querySelector(".author");
 const imgDiv = document.querySelector(".image-container");
 const img = document.querySelector(".img");
+const url = "https://picsum.photos/v2/list?limit=100";
+
+const getImage = async () => {
+    const res = await fetch(url);
+    const image = await res.json();
+    console.log(image);
+}
+
+getImage();
